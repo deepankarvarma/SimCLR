f0ff6279baf18019a55469d74492b84cf9f29560
Deepankar Varma <72309699+deepankarvarma@users.noreply.github.com>
2024-01-19 22:28:28 +0530

    Update model.py

--- model.py
+++ model.py
@@ -5,21 +5,6 @@ from torchvision.models.resnet import resnet50
 from collections import OrderedDict
 
 
-# class Model(nn.Module):
-#     def __init__(self, feature_dim=128):
-#         super(Model, self).__init__()
-
-#         self.f = []
-#         for name, module in resnet50().named_children():
-#             if name == 'conv1':
-#                 module = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-#             if not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
-#                 self.f.append(module)
-#         # encoder
-#         self.f = nn.Sequential(*self.f)
-#         # projection head
-#         self.g = nn.Sequential(nn.Linear(2048, 512, bias=False), nn.BatchNorm1d(512),
-#                                nn.ReLU(inplace=True), nn.Linear(512, feature_dim, bias=True))
 class Model(nn.Module):
     def __init__(self, feature_dim=128):
         super(Model, self).__init__()
@@ -28,15 +13,14 @@ class Model(nn.Module):
         for name, module in resnet50().named_children():
             if name == 'conv1':
                 module = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-                self.f.append(('conv1', module))  # Match the name 'conv1'
-            elif not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
-                self.f.append((name, module))  # Match other names
-
+            if not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
+                self.f.append(module)
         # encoder
-        self.f = nn.Sequential(OrderedDict(self.f))
+        self.f = nn.Sequential(*self.f)
         # projection head
         self.g = nn.Sequential(nn.Linear(2048, 512, bias=False), nn.BatchNorm1d(512),
                                nn.ReLU(inplace=True), nn.Linear(512, feature_dim, bias=True))
+
 
     def forward(self, x):
         x = self.f(x)

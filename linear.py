135502a3e78b0a747f52db61073691c5a2e024cc
Deepankar Varma <72309699+deepankarvarma@users.noreply.github.com>
2024-01-22 14:53:10 +0530

    Update linear.py

--- linear.py
+++ linear.py
@@ -257,35 +257,12 @@ class Net(nn.Module):
         out = self.fc1(feature)
         return out
 
-def train_val(net, data_loader, train_optimizer):
-    is_train = train_optimizer is not None
-    net.train() if is_train else net.eval()
-
-    total_loss, total_correct_1, total_correct_5, total_num, data_bar = 0.0, 0.0, 0.0, 0, tqdm(data_loader)
-    
-    with (torch.enable_grad() if is_train else torch.no_grad()):
-        for data, target in data_bar:
-            data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
-            out = net(data)
-            loss = loss_criterion(out, target)
-
-            if is_train:
-                train_optimizer.zero_grad()
-                loss.backward()
-                train_optimizer.step()
-
-            total_num += data.size(0)
-            total_loss += loss.item() * data.size(0)
-            prediction = torch.argsort(out, dim=-1, descending=True)
-            total_correct_1 += torch.sum((prediction[:, 0:1] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
-            total_correct_5 += torch.sum((prediction[:, 0:5] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
-
-            data_bar.set_description('{} Epoch: [{}/{}] Loss: {:.4f} ACC@1: {:.2f}% ACC@5: {:.2f}%'
-                                     .format('Train' if is_train else 'Test', epoch, epochs,
-                                             total_loss / total_num, total_correct_1 / total_num * 100,
-                                             total_correct_5 / total_num * 100))
-
-    return total_loss / total_num, total_correct_1 / total_num * 100, total_correct_5 / total_num * 100
+def closure():
+    optimizer.zero_grad()
+    out = model(data)
+    loss = loss_criterion(out, target)
+    loss.backward()
+    return loss
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Linear Evaluation')

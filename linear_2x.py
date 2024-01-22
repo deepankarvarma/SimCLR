56a55731067100aa9fcd0a7dfb3ab6da11cafc3e
Deepankar Varma <72309699+deepankarvarma@users.noreply.github.com>
2024-01-22 15:32:59 +0530

    Update linear_2x.py

--- linear_2x.py
+++ linear_2x.py
@@ -1,172 +1,43 @@
-# import argparse
-
-# import pandas as pd
-# import torch
-# import torch.nn as nn
-# import torch.optim as optim
-# from thop import profile, clever_format
-# from torch.utils.data import DataLoader
-# from torchvision.datasets import CIFAR10
-# from tqdm import tqdm
-
-# import utils
-# from model import Model
-
-
-# class Net(nn.Module):
-#     def __init__(self, num_class, pretrained_path):
-#         super(Net, self).__init__()
-
-#         # encoder
-#         self.net = Model().net
-#         # classifier
-#         self.fc1 = nn.Linear(2048, num_class, bias=True)
-#         pretrained_dict=torch.load(pretrained_path, map_location='cpu')
-#         # print("Pretrained Dict",pretrained_dict['resnet'].keys())
-#         # print("Model Dict",self.state_dict().keys())
-#         self.load_state_dict(pretrained_dict['resnet'], strict=False)
-
-#     def forward(self, x):
-#         x = self.net(x)
-#         feature = torch.flatten(x, start_dim=1)
-#         out = self.fc1(feature)
-#         return out
-
-
-# # train or test for one epoch
-# def train_val(net, data_loader, train_optimizer):
-#     is_train = train_optimizer is not None
-#     net.train() if is_train else net.eval()
-
-#     total_loss, total_correct_1, total_correct_5, total_num, data_bar = 0.0, 0.0, 0.0, 0, tqdm(data_loader)
-#     with (torch.enable_grad() if is_train else torch.no_grad()):
-#         for data, target in data_bar:
-#             data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
-#             out = net(data)
-#             loss = loss_criterion(out, target)
-
-#             if is_train:
-#                 train_optimizer.zero_grad()
-#                 loss.backward()
-#                 train_optimizer.step()
-
-#             total_num += data.size(0)
-#             total_loss += loss.item() * data.size(0)
-#             prediction = torch.argsort(out, dim=-1, descending=True)
-#             total_correct_1 += torch.sum((prediction[:, 0:1] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
-#             total_correct_5 += torch.sum((prediction[:, 0:5] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
-
-#             data_bar.set_description('{} Epoch: [{}/{}] Loss: {:.4f} ACC@1: {:.2f}% ACC@5: {:.2f}%'
-#                                      .format('Train' if is_train else 'Test', epoch, epochs, total_loss / total_num,
-#                                              total_correct_1 / total_num * 100, total_correct_5 / total_num * 100))
-
-#     return total_loss / total_num, total_correct_1 / total_num * 100, total_correct_5 / total_num * 100
-
-
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser(description='Linear Evaluation')
-#     parser.add_argument('--model_path', type=str, default='results/128_0.5_200_512_500_model.pth',
-#                         help='The pretrained model path')
-#     parser.add_argument('--batch_size', type=int, default=512, help='Number of images in each mini-batch')
-#     parser.add_argument('--epochs', type=int, default=100, help='Number of sweeps over the dataset to train')
-
-#     args = parser.parse_args()
-#     model_path, batch_size, epochs = args.model_path, args.batch_size, args.epochs
-#     train_data = CIFAR10(root='data', train=True, transform=utils.train_transform, download=True)
-#     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=16, pin_memory=True)
-#     test_data = CIFAR10(root='data', train=False, transform=utils.test_transform, download=True)
-#     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True)
-
-#     model = Net(num_class=len(train_data.classes), pretrained_path=model_path).cuda()
-#     for param in model.net.parameters():
-#         param.requires_grad = False
-
-#     flops, params = profile(model, inputs=(torch.randn(1, 3, 32, 32).cuda(),))
-#     flops, params = clever_format([flops, params])
-#     print('# Model Params: {} FLOPs: {}'.format(params, flops))
-#     optimizer = optim.Adam(model.fc1.parameters(), lr=1e-4, weight_decay=1e-6)
-#     loss_criterion = nn.CrossEntropyLoss()
-#     results = {'train_loss': [], 'train_acc@1': [], 'train_acc@5': [],
-#                'test_loss': [], 'test_acc@1': [], 'test_acc@5': []}
-
-#     best_acc = 0.0
-#     for epoch in range(1, epochs + 1):
-#         train_loss, train_acc_1, train_acc_5 = train_val(model, train_loader, optimizer)
-#         results['train_loss'].append(train_loss)
-#         results['train_acc@1'].append(train_acc_1)
-#         results['train_acc@5'].append(train_acc_5)
-#         test_loss, test_acc_1, test_acc_5 = train_val(model, test_loader, None)
-#         results['test_loss'].append(test_loss)
-#         results['test_acc@1'].append(test_acc_1)
-#         results['test_acc@5'].append(test_acc_5)
-#         # save statistics
-#         data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
-#         data_frame.to_csv('results/linear_statistics.csv', index_label='epoch')
-#         if test_acc_1 > best_acc:
-#             best_acc = test_acc_1
-#             torch.save(model.state_dict(), 'results/linear_model.pth')
-
-
-
-import argparse
-import pandas as pd
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.optim.lr_scheduler import CosineAnnealingLR
-from thop import profile, clever_format
+import torchvision.models as models
+import argparse
+import pandas as pd
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 from tqdm import tqdm
+from thop import profile, clever_format
 import utils
-from model_2x import Model  # Assuming you have a custom model defined in 'model.py'
-
+from model import Model
 class Net(nn.Module):
     def __init__(self, num_class, pretrained_path):
         super(Net, self).__init__()
 
-        # encoder
-        self.net = Model().net
-        # classifier
+        # Create a model to get the architecture
+        model=Model()
+        self.net = model.net
         self.fc1 = nn.Linear(4096, num_class, bias=True)
-        pretrained_dict = torch.load(pretrained_path, map_location='cpu')
-        self.load_state_dict(pretrained_dict['resnet'], strict=False)
+
+        # Load pretrained weights
+        pretrained_dict = torch.load(pretrained_path, map_location='cpu')['resnet']
+        model_dict = self.state_dict()
+
+        # Filter out unnecessary keys
+        pretrained_dict = {k[len("net."):]: v for k, v in pretrained_dict.items() if k.startswith("net.")}
+
+        # Update only matching layers
+        for key in model_dict.keys():
+            if key in pretrained_dict and model_dict[key].shape == pretrained_dict[key].shape:
+                model_dict[key] = pretrained_dict[key]
+
+        # Load the modified state dict
+        self.load_state_dict(model_dict, strict=False)
 
     def forward(self, x):
         x = self.net(x)
         feature = torch.flatten(x, start_dim=1)
         out = self.fc1(feature)
         return out
-
-def train_val(net, data_loader, train_optimizer):
-    is_train = train_optimizer is not None
-    net.train() if is_train else net.eval()
-
-    total_loss, total_correct_1, total_correct_5, total_num, data_bar = 0.0, 0.0, 0.0, 0, tqdm(data_loader)
-    
-    with (torch.enable_grad() if is_train else torch.no_grad()):
-        for data, target in data_bar:
-            data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
-            out = net(data)
-            loss = loss_criterion(out, target)
-
-            if is_train:
-                train_optimizer.zero_grad()
-                loss.backward()
-                train_optimizer.step()
-
-            total_num += data.size(0)
-            total_loss += loss.item() * data.size(0)
-            prediction = torch.argsort(out, dim=-1, descending=True)
-            total_correct_1 += torch.sum((prediction[:, 0:1] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
-            total_correct_5 += torch.sum((prediction[:, 0:5] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
-
-            data_bar.set_description('{} Epoch: [{}/{}] Loss: {:.4f} ACC@1: {:.2f}% ACC@5: {:.2f}%'
-                                     .format('Train' if is_train else 'Test', epoch, epochs,
-                                             total_loss / total_num, total_correct_1 / total_num * 100,
-                                             total_correct_5 / total_num * 100))
-
-    return total_loss / total_num, total_correct_1 / total_num * 100, total_correct_5 / total_num * 100
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Linear Evaluation')
@@ -193,8 +64,8 @@ if __name__ == '__main__':
     flops, params = clever_format([flops, params])
     print('# Model Params: {} FLOPs: {}'.format(params, flops))
     
-    optimizer = optim.SGD(model.fc1.parameters(), lr=0.01, momentum=0.9, weight_decay=0, nesterov=True)
-    scheduler = CosineAnnealingLR(optimizer, T_max=epochs)
+    optimizer = torch.optim.SGD(model.fc1.parameters(), lr=0.01, momentum=0.9, weight_decay=0, nesterov=True)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs)
     
     loss_criterion = nn.CrossEntropyLoss()
     
